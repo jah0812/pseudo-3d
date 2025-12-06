5d6aba248fc08b8020119cf57e73dfb3adb38658
jah0812 <jaredtrpplnine@gmail.com>
2025-12-06 16:15:50 +0300

    another commit

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import pygame
+from settings import *
+from player import Player
+import math
+from map import world_map
+
 
 pygame.init()
 
-sc = pygame.display.set_mode(())
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+player = Player()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    player.movement()
+    sc.fill(BLACK)
+
+    pygame.draw.circle(sc, GREEN, player.pos, 12)
+    pygame.draw.line(sc, GREEN, player.pos, (player.x + WIDTH * math.cos(player.angle), player.y + WIDTH * math.sin(player.angle)))
+
+    for x, y in world_map:
+        pygame.draw.rect(sc, DARKGRAY, (x, y, TILE, TILE))
+
+    pygame.display.flip()
+    clock.tick(FPS)
